fb7f01356d031a133956598a9d6f4a91a9a95e0c
Rhiday <shahriar.rhiday@gmail.com>
2019-07-23 03:49:06 +0300

    fixed grid creation logic

--- app.js
+++ app.js
@@ -1,43 +1,59 @@
 
-function grid() {
-        /*let y = document.querySelector('.grid-input').value;*/
-
-        let x = 10;
+function grid(gridCount) {
 
         const container = document.querySelector('.container');
 
-        for (let row = 0; row <= x; row++) {
-
-            const divRow = document.createElement('div');
-
-            for (let cell = 0; cell <= x; cell++) {    
-                
-                const div = document.createElement('div');
-
-                div.style.display= 'inline-block';
-                div.style.height = '0';
-                div.style.width = '5%';
-                div.style.paddingBottom = '5%';
-                div.style.border= ('1px solid black');
-                div.style.backgroundColor =('lightpink');
-
-                divRow.appendChild(div);
-
-                div.addEventListener('mouseover', () => {
-                    div.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-                    //div.style.backgroundColor = 'inherit';
-
-                });
-        }
+        container.innerHTML = "";  //clear the grid
         
-        container.appendChild(divRow);
-    }
+        if (gridCount != 0) {
+
+            for (let row = 0; row < gridCount; row++) {
+
+                const gridRow = document.createElement('div');
+                gridRow.style.lineHeight = '0';
+    
+                for (let cell = 0; cell < gridCount; cell++) {    
+                    
+                    const div = document.createElement('div');
+    
+                    div.style.display= 'inline-block';
+                    div.style.lineHeight= '0';
+                    div.style.height = '0';
+                    div.style.width = '5%';
+                    div.style.paddingBottom = '5%';
+                    div.style.border= ('1px solid black');
+                    div.style.backgroundColor =('lightpink');
+    
+                    gridRow.appendChild(div);
+    
+                    div.addEventListener('mouseover', () => {
+                        div.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    
+    
+                    });
+            }
+            
+            container.appendChild(gridRow);
+        }
+     }        
+
 
 }
 
-const gridSize = document.querySelector('.generate-grid');
 
-gridSize.addEventListener('click', grid());
+const gridSize = document.querySelector('.generate-grid');
+const gridClear = document.querySelector('.grid-clear');
+
+window.addEventListener('load', () => {
+    grid(8);
+});
+
+gridSize.addEventListener('click', () => {
+    const gridInput = document.querySelector('.grid-input').value;
+    grid(gridInput);
+});
+
+
 
 
 
@@ -48,3 +64,5 @@ if (row == x) {
 }
 
 */
+
+  /*let y = document.querySelector('.grid-input').value;*/
